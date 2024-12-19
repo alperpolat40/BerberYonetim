f0f73fb5e32b503cfb8f87e01e5778fd3580e5a8
alper polat <alper.polat1@ogr.sakarya.edu.tr>
2024-12-19 12:56:39 +0300

    5.commit: Admin paneli oluşturuldu: Rol ve kullanıcı yönetimi tamamlandı.

--- BerberYonetim/Program.cs
+++ BerberYonetim/Program.cs
@@ -1,13 +1,23 @@
 using BerberYonetim.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BerberYonetim.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>() // Rol yönetimi için ekleme
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 // Add services to the container.
+var app = builder.Build();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await RoleInitializer.SeedRolesAndAdminAsync(services);
+}
 
 // Configure database connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
